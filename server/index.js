0fdb0cd3cb5ce75473668a3d03f1573b3f29eed4
Stephen-Roach <stephenroach21@gmail.com>
2021-09-17 10:44:33 -0700

    added keys and callback url for authentication

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-passport.use(new GoogleStrategy()); //creates a new instance of the google strategy
+//creates a new instance of the google strategy which takes in the authentication keys as well as a callback URL
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken); //access token given to us once we are authorized
+    }
+  )
+);
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there!' });
