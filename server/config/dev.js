2732a3377f4daa689126de41c947401239ea7722
Stephen-Roach <stephenroach21@gmail.com>
2021-09-21 13:37:23 -0700

    .gitignore is now working

--- server/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '566930515489-j4gv95mjpj33ch52f3keadfs4n5ul2r7.apps.googleusercontent.com',
-  googleClientSecret: 'dFkJqruu9PnceBSP2R32I4GH',
-  mongoURI:
-    'mongodb+srv://Sdaven:Java123@emaily.y0ggk.mongodb.net/test?retryWrites=true&w=majority',
-  cookieKey: 'sdfsdfasdfasdfasdad',
-};
