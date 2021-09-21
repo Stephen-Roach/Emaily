bbadec549ceb5cdee337f1032881168c210c7d03
Stephen-Roach <stephenroach21@gmail.com>
2021-09-21 12:59:42 -0700

    refactored keys so that prod and dev have their own set

---
+++ server/config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '566930515489-j4gv95mjpj33ch52f3keadfs4n5ul2r7.apps.googleusercontent.com',
+  googleClientSecret: 'dFkJqruu9PnceBSP2R32I4GH',
+  mongoURI:
+    'mongodb+srv://Sdaven:Java123@emaily.y0ggk.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'sdfsdfasdfasdfasdad',
+};
