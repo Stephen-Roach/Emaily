74b02b32c32116cb94a434702e2364fd64713bc4
Stephen-Roach <stephenroach21@gmail.com>
2021-09-22 13:20:12 -0700

    now it is fixed

---
+++ routes/authRoutes.js
@@ -0,0 +1,22 @@
+const { get } = require('mongoose');
+const passport = require('passport');
+
+module.exports = (app) => {
+  app.get(
+    '/auth/google', //once a user goes to this domain, they will get kicked to the oauth flow
+    passport.authenticate('google', {
+      scope: ['profile', 'email'],
+    })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+};
