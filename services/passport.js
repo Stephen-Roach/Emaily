afd79c7b70def5d1bd6a23cd0584b42f9f9eb2fd
Stephen-Roach <stephenroach21@gmail.com>
2021-09-22 12:49:25 -0700

    changed congif key structure

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
-// const keys = require('./config/keys');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users'); // fetching the users from the mongoose collection
@@ -25,6 +24,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
+      proxy: true, // fixes the security issue with the heroku http vs https domain
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
