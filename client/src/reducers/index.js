5c0d04c22cbe593fc5b2f2c85221a3fe034551a9
Stephen-Roach <stephenroach21@gmail.com>
2021-09-24 08:15:47 -0700

    added auth reducer

---
+++ client/src/reducers/index.js
@@ -0,0 +1,7 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowAnonymousFunction": true}] */
+export default function (state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
