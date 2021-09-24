62a011b8ef324d1eafb414e0739ec0863fd2da57
Stephen-Roach <stephenroach21@gmail.com>
2021-09-24 09:58:26 -0700

    added navbar to header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <div className='nav-wrapper'>
+        <a href='/' className=' left brand-logo'>
+          Emaily
+        </a>
+        <ul id='nav-mobile' class='right hide-on-med-and-down'>
+          <li>
+            <a href='sass.html'>Login with Google</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
